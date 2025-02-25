33a13817a23712750b269d87c4830084094078c0
BayronP33 <162054798+BayronP33@users.noreply.github.com>
2025-02-25 14:58:50 -0500

    Add files via upload

---
+++ Collecctions/src/collecctions/Collecctions.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package collecctions;
+
+import java.util.*;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Collecctions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ sockets-master/sockets-master/src/socket/control.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package socket;
+
+/**
+ *
+ * @author locon
+ */
+public class control {
+    
+    public static void main(String[] args) {
+        
+        Mensajeria chat = new Mensajeria();
+        //socket1 cliente = new socket1();
+        //socket2 server = new socket2();
+
+    }
+    
+}
